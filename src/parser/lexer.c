d8dbbad0e5b009724af9991d5e6ceb73dc64352d
angelo-digian <angelo.digianvito@people-first.co>
2024-12-06 19:00:01 +0100

    Added expansions

--- src/parser/lexer.c
+++ src/parser/lexer.c
@@ -1,20 +1,20 @@
 #include "../../includes/minishell.h"
 
-int	int_clean_input(char *str)
+int int_clean_input(char *str)
 {
 	int i;
-	int	count;
+	int count;
 
 	i = 0;
 	count = 0;
-	while ((str[i] >= 9 && str[i] <= 13) || str[i]==' ')
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
 		i++;
 	while (str[i] != '\0')
 	{
 		if (str[i] == '$')
 		{
-			while ((str[i]<9||str[i]>13) && str[i] != ' ')
-				i++; 
+			// Use the expand_variable_length function to get the length of the expanded variable
+			count += expand_variable_length(str, &i);
 		}
 		else if (str[i] == '"')
 		{
@@ -23,10 +23,7 @@ int	int_clean_input(char *str)
 			while (str[i] != '\"')
 			{
 				if (str[i] == '$')
-				{
-					while ((str[i]<9||str[i]>13) && str[i] != ' ')
-						i++; 
-				}
+					count += expand_variable_length(str, &i);
 				i++;
 				count++;
 			}
@@ -45,7 +42,7 @@ int	int_clean_input(char *str)
 			i++;
 			count++;
 		}
-		else if (((str[i] >= 9 && str[i] <= 13) || str[i]==' ')&&((str[i+1] >= 9 && str[i+1] <= 13) || str[i+1] == ' ' || str[i+1] == '\0'))
+		else if (is_tabular(&str[i], &str[i + 1]))
 			i++;
 		else
 		{
@@ -53,129 +50,55 @@ int	int_clean_input(char *str)
 			count++;
 		}
 	}
-	return(count);
+	return (count);
 }
 
-// Every iteration can be done in functions sending pointers
 char *lexer(char *str)
 {
 	int i;
 	int j;
-	int	to_expand;
 	char *str_clean;
 
+	j = 0;
+	str_clean = malloc(sizeof(char) * (int_clean_input(str) + 1));
+	if (!str_clean)
+		return (NULL);
 	i = 0;
-	str_clean = malloc(sizeof (int) * int_clean_input(str));
-	while ((str[i] >= 9 && str[i] <= 13) || str[i]==' ')
+	// Skip initial whitespace
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
 		i++;
+
+	// Process the string
 	while (str[i] != '\0')
 	{
 		if (str[i] == '$')
 		{
-			to_expand = 0;
-			while ((str[i]<9||str[i]>13) && str[i] != ' ')
-			{	
-				i++;
-				to_expand++;
-			}
-			// expander(str, &i, &to_expand, _env);
+			// Expand environment variable
+			expand_variable(str, &i, str_clean, &j);
 		}
 		else if (str[i] == '\"')
 		{
-			str_clean[j] = str[i];
-			i++;
-			j++;
-			while (str[i] != '\"')
-			{
-				if (str[i] == '$')
-				{
-					to_expand = 0;
-					while ((str[i]<9||str[i]>13) && str[i] != ' ')
-					{	
-						i++;
-						to_expand++;
-					}
-					// expander(str, &i, &to_expand, _env);
-				}
-				str_clean[j] = str[i];
-				i++;
-				j++;
-				if (str[i] == '\0')
-					return ("ERROR-CLOSE DOUBLE QUOTES");
-			}
-			str_clean[j] = str[i];
-			i++;
-			j++;
+			// Handle double quotes
+			if (handle_double_quotes(str, str_clean, &i, &j) == -1)
+				return (NULL);
 		}
 		else if (str[i] == '\'')
 		{
-			str_clean[j] = str[i];
-			i++;
-			j++;
-			while (str[i] != '\'')
-			{
-				str_clean[j] = str[i];
-				i++;
-				j++;
-				if (str[i] == '\0')
-					return ("ERROR-CLOSE SINGLE QUOTES");
-			}
-			str_clean[j] = str[i];
+			// Handle single quotes
+			if (handle_single_quotes(str, str_clean, &i, &j) == -1)
+				return (NULL);
+		}
+		else if (is_tabular(str[i], str[i + 1]))
+		{
 			i++;
 		}
-		else if (((str[i] >= 9 && str[i] <= 13) || str[i]==' ')&&((str[i+1] >= 9 && str[i+1] <= 13) || str[i+1] == ' ' || str[i+1] == '\0'))
-			i++;
 		else
 		{
-			str_clean[j] = str[i];
-			i++;
-			j++;
+			// Copy regular characters
+			str_clean[j++] = str[i++];
 		}
 	}
+
 	str_clean[j] = '\0';
 	return (str_clean);
-}
-
-
-// Other suggested functions
-int check_double_single_quote(char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] == '"')
-		{
-			while (str[i] != '\0')
-			{
-				if(str[i] == '"')
-					break;
-				i++;
-			}
-			return (0);
-		}
-		if (str[i] == '\'')
-		{
-			while (str[i] != '\0')
-			{
-				if(str[i] == '\'')
-					break;
-				i++;
-			}
-			return (0);
-		}
-		i++;
-	}
-	return (1);
-}
-
-char *double_quote()
-{
-
-}
-
-char *single_quote()
-{
-
 }
--- src/parser/utils.c
+++ src/parser/utils.c
@@ -1,8 +1,126 @@
 #include "../../includes/minishell.h"
 
+void    skip_word(int *i, char *str)
+{
+    while ((str[*i] < 9 || str[*i] > 13) && str[*i] != ' ')
+        (*i)++;
+}
+
+int    is_tabular(char c, char d)
+{
+    if (((c >= 9 && c <= 13) || c == ' ') && ((d >= 9 && d <= 13) || d == ' ' || d == '\0'))
+        return (1);
+    else
+        retrun (0);
+}
+
+// Helper function for handling double quotes
+int handle_double_quotes(char *str, char *str_clean, int *i, int *j)
+{
+    int to_expand;
+
+    str_clean[(*j)++] = str[(*i)++]; // Copy the opening quote
+    while (str[*i] != '\"' && str[*i] != '\0')
+    {
+        if (str[*i] == '$')
+			expander(str, i);
+        else
+            str_clean[(*j)++] = str[(*i)++]; // Copy character inside quotes
+    }
+    if (str[*i] == '\"') // Closing double quote
+        str_clean[(*j)++] = str[(*i)++];
+    else // Error: unclosed double quote
+        return -1;
+    return 0;
+}
+
+// Helper function for handling single quotes
+int handle_single_quotes(char *str, char *str_clean, int *i, int *j)
+{
+    str_clean[(*j)++] = str[(*i)++]; // Copy the opening quote
+
+    while (str[*i] != '\'' && str[*i] != '\0')
+        str_clean[(*j)++] = str[(*i)++];
+    if (str[*i] == '\'') // Closing single quote
+        str_clean[(*j)++] = str[(*i)++];
+    else // Error: unclosed single quote
+        return -1;
+    return 0;
+}
+
 void start(t_general *all, char **build)
 {
     all->env_v = build_env(build);
     all->export_v = build_env(build);
     //all->tmp = "\n";
+}
+
+void expand_variable(char *str, int *i, char *str_clean, int *j)
+{
+	char *var_name;
+	char *var_value;
+	int k;
+	int l;
+
+	var_name = malloc(sizeof(char) * 256);
+	if (!var_name)
+		return (0);
+
+	// Skip the '$' character
+	(*i)++;
+	k = 0;
+	// Extract variable name
+	while (str[*i] && (isalnum(str[*i]) || str[*i] == '_'))
+	{
+		if (k < 255)
+		{
+			var_name[k] = str[*i];
+			k++;
+		}
+		(*i)++;
+	}
+	var_name[k] = '\0';
+
+	// Get the variable value from the environment
+	var_value = getenv(var_name);
+
+	// If variable exists, append its value to str_clean
+	if (var_value)
+	{
+		l = 0;
+		while (var_value[l])
+		{
+			str_clean[(*j)++] = var_value[l];
+			l++;
+		}
+	}
+	// If variable does not exist, do nothing (empty expansion)
+	free(var_name);
+}
+
+int expand_variable_length(char *str, int *i)
+{
+	char var_name[256];
+	char *var_value;
+	int k;
+
+	(*i)++; // Skip the '$' character
+
+	// Extract variable name
+	while (str[*i] && (isalnum(str[*i]) || str[*i] == '_'))
+	{
+		if (k < 255) // Prevent buffer overflow
+			var_name[k++] = str[*i];
+		(*i)++;
+	}
+	var_name[k] = '\0';
+
+	// Get the variable value from the environment
+	var_value = getenv(var_name);
+
+	// Return the length of the expanded value, or 0 if the variable doesn't exist
+	if (var_value)
+		return strlen(var_value);
+	else
+		return 0;
 }
